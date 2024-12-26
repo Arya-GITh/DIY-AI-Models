d32661d873a94a066071839307992fe5840dedd1
Arya Datla <150571926+Arya-GITh@users.noreply.github.com>
2024-12-26 05:36:45 -0500

    Update KNN.py
    Added vectorized version

--- KNN.py
+++ KNN.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
+from scipy.stats import mode
 digits = load_digits()
 
 X = pd.DataFrame(digits.data)
@@ -11,44 +12,46 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.2)
 
 X_train = X_train.values
 X_test = X_test.values
-
+Y_train = Y_train.values
+Y_test = Y_test.values
 
 
 #helpers
-def euclidean(x1,x2):
-    return np.sqrt(np.sum((x1-x2)**2))
+def euclidean(X_train,X_test):
+    
+    X_train_2 = np.sum(np.square(X_train),axis=1)
+    X_test_2 = np.sum(np.square(X_test),axis=1)
+    cross = np.dot(X_test,X_train.T)
+    dist_2 = X_test_2[:,np.newaxis] + X_train_2 - 2*cross
+    dist =  np.sqrt(np.maximum(dist_2,0))
+    return dist
 
 #KNN Classifcation
 def knn_classify(X_train, X_test, Y_train,k):
-    Y_predict = []
-    for x in X_test:
-        dist = []
-        for z in X_train:
-            dist.append(euclidean(x,z))
-        dist = sorted(list(enumerate(dist)), key = lambda a:a[1])
-        k_indices = [pair[0] for pair in dist[:k]]
-        y_k = Y_train.iloc[k_indices]
-        Y_predict.append(y_k.mode().iloc[0])
+
+    dist = euclidean(X_train,X_test)
+    indices = np.argsort(dist,axis=1)[:,:k]
+    y_k = Y_train[indices]
+    Y_predict = mode(y_k, axis=1).mode.flatten()
     
     return Y_predict
 
 def score(Y_predict,Y_test):
-    sum = 0
-    for x,y in zip(Y_predict,Y_test.values.ravel()):
-        if x == y:
-            sum += 1
-    score = sum/len(Y_test)
+    score = np.mean(Y_predict == Y_test)
     return score
 
 Y_predict = knn_classify(X_train,X_test,Y_train,10)
 
 print(score(Y_predict,Y_test))
 
-k_val = [8,9,10,11,12]
+k_val = range(1,20)
 
 pred_val = []
 for k in k_val:
     pred_val.append(score(knn_classify(X_train, X_test, Y_train,k), Y_test))
 
-plt.scatter(k_val,pred_val)
+plt.plot(k_val,pred_val)
+plt.ylabel("accuracy")
+plt.xlabel("k-value")
+plt.title("finding the optimal k")
 plt.show()
